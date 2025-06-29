655a7b89c47029271a93f00906a3038918715939
Dymirt <kolidadmytro@gmail.com>
2025-06-29 22:17:12 +0200

    removing bootstrap
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- header.php
+++ header.php
@@ -19,8 +19,6 @@ $count = WC()->cart->get_cart_contents_count();
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<link rel="profile" href="http://gmpg.org/xfn/11">
 	<link rel="pingback" href="<?php bloginfo('pingback_url'); ?>">
-	<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.7/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-LN+7fdVzj6u52u30Kp6M/trliBMCMKTyK833zpbD+pXdCLuTusPj697FH4R/5mcr" crossorigin="anonymous">
-
 	<?php wp_head(); ?>
 
 </head>
