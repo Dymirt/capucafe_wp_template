ba97f1865652d34e3089acb2b4dd6e1a7901c72c
Dymirt <kolidadmytro@gmail.com>
2025-07-06 13:41:45 +0200

    fixing header
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- headers/header-shop.php
+++ headers/header-shop.php
@@ -1,4 +1,4 @@
-<div data-property-1="mobile 2" class="header-shop z-3 fixed h-[100px] flex items-center w-screen px-[5%] justify-between ">
+<div data-property-1="mobile 2" class="header-shop z-3 fixed h-[100px] flex items-center w-screen px-[5%] justify-between top-0">
 	<div class="max-w-40 inline-flex flex-col justify-start items-start overflow-hidden">
 		<div class="w-28 h-8 relative overflow-hidden">
 			<a href="<?php echo esc_url(home_url('/')); ?>">
