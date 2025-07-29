8b68caf16799d3deb3ae5bd12c6da11f19873d0a
Dymirt <kolidadmytro@gmail.com>
2025-07-29 23:38:09 +0200

    footer color
    Signed-off-by: Dymirt <kolidadmytro@gmail.com>

--- footer.php
+++ footer.php
@@ -28,7 +28,7 @@
 		// do_action( 'storefront_footer' );
 		?>
 
-		<div data-property-1="mobile v2" class="w-full self-stretch px-5 md:px-32 pt-36 md:pt-24 relative bg-stone-700 md:flex justify-start content-start items-start gap-7">
+		<div data-property-1="mobile v2" class="w-full self-stretch px-5 md:px-32 pt-36 md:pt-24 relative bg-[#42352F] md:flex justify-start content-start items-start gap-7">
 			<div class="w-48 min-w-44 h-full basis-auto mb-4">
 				<?php echo get_custom_logo(); ?>
 			</div>
